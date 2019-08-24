e72feaefec281984b79a7cade2aa1fd02260e1ae
rockingrahul98 <rockingrahul98@gmail.com>
2019-08-24 21:53:14 +0530

    added variable in mainactivity

--- app/src/main/java/io/madmachines/recyclerviewexample/MainActivity.java
+++ app/src/main/java/io/madmachines/recyclerviewexample/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView tv= (TextView) findViewById(R.id.hello);
         tv.setText("Git hub");
+        int x=5;
     }
 }
